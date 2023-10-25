b26bff3c1717cc7f14b2120206a326c799fe920c
Sanjeevviswanath <sanjeev.viswanath@gmail.com>
2023-10-24 22:44:24 -0700

    Build a responsive layout using chakra

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg={"coral"}>
+        nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"gold"}>
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"dodgerblue"}>
+        main
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
