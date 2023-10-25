740ebeadcdf72a3b6370a993258ed6dc9913fe5a
Sanjeevviswanath <sanjeev.viswanath@gmail.com>
2023-10-24 23:44:41 -0700

    Build the color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/nextgen-logo.svg";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack padding="10px" justifyContent={"space-between"}>
       <Image src={logo} boxSize="60px"></Image>
-      <Text>Navbar</Text>
+      <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
 };
