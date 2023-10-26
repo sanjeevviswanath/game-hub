a93da1f741f40dbd72c689f070bb905c697b2d3c
Sanjeevviswanath <sanjeev.viswanath@gmail.com>
2023-10-26 11:23:37 -0700

    implemented AbortController to manage the case of aborting the request while unmounting

--- src/hooks/UseGames.ts
+++ src/hooks/UseGames.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
 
 interface Games {
   id: number;
@@ -13,16 +14,23 @@ interface FetGamesResponse {
 const UseGames = () => {
   const [games, setGames] = useState<Games[]>([]);
   const [error, setError] = useState("");
+  
 
   useEffect(() => {
+    const controller = new AbortController();
     apiClient
-      .get<FetGamesResponse>("/games")
+      .get<FetGamesResponse>("/games", { signal: controller.signal })
       .then((res) => {
         setGames(res.data.results);
       })
       .catch((err) => {
+        if (err instanceof CanceledError) return;
         setError(err.message);
       });
+    return () => {
+      //cleanup function
+      controller.abort();
+    };
   }, []);
 
   return { games, error };
