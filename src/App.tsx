3bca315f6775ebee05e3b73c466183e80b4ff583
Sanjeevviswanath <sanjeev.viswanath@gmail.com>
2023-10-26 20:56:08 -0700

    commit to vercel push

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem area={"aside"}>left</GridItem>
+        <GridItem area={"aside"}>.</GridItem>
       </Show>
       <GridItem area={"main"}>
         <GameGrid></GameGrid>
