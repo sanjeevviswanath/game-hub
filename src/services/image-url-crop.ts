f833b2f2f4deba756606e9bc72455751626fe4c3
Sanjeevviswanath <sanjeev.viswanath@gmail.com>
2023-10-27 00:17:23 -0700

    refactored the code

--- src/services/image-url-crop.ts
+++ src/services/image-url-crop.ts
@@ -1,5 +1,6 @@
 const getCroppedImageUrl = (url: string): string => {
-  const index = url.indexOf("media/") + "media/".length;
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
 
