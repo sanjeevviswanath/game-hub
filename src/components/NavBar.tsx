43e50e61108c1824022695034bc40f176eee7ec0
Sanjeevviswanath <sanjeev.viswanath@gmail.com>
2023-10-28 11:08:51 -0700

    updated page title font styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ const NavBar = () => {
   return (
     <HStack padding="10px" justifyContent={"space-between"}>
       <Image src={logo} boxSize="60px"></Image>
-      <Text fontSize={"xxx-large"}>GamePulse: Ratings & Reviews</Text>
+      <Text fontSize={"xxx-large"} fontWeight={"thin"}>GamePulse</Text>
       <ColorModeSwitch />
     </HStack>
   );
